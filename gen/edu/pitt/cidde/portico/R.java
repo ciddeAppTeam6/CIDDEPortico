75a2f929d93cf980dca17ff0968c8b69722b5990
David Yu <day21@pitt.edu>
2013-03-22 14:37:09 -0400

    David's screens

--- gen/edu/pitt/cidde/portico/R.java
+++ gen/edu/pitt/cidde/portico/R.java
@@ -24,29 +24,71 @@ public final class R {
         public static final int vga_inputs=0x7f02000a;
     }
     public static final class id {
-        public static final int btnStub1=0x7f070000;
-        public static final int btnStub2=0x7f070001;
-        public static final int btnStub3=0x7f070002;
-        public static final int gridview=0x7f070003;
-        public static final int menu_settings=0x7f070006;
-        public static final int pager=0x7f070004;
-        public static final int pager_title_strip=0x7f070005;
+        public static final int app_help=0x7f070027;
+        public static final int btnStub1=0x7f070018;
+        public static final int btnStub2=0x7f070019;
+        public static final int btnStub3=0x7f07001a;
+        public static final int button1=0x7f070000;
+        public static final int button2=0x7f07001d;
+        public static final int button3=0x7f07001f;
+        public static final int editText1=0x7f07001c;
+        public static final int editText2=0x7f07001e;
+        public static final int gridview=0x7f07001b;
+        public static final int menu_settings=0x7f070023;
+        public static final int pager=0x7f070021;
+        public static final int pager_title_strip=0x7f070022;
+        public static final int prompt=0x7f070020;
+        public static final int return_home=0x7f070024;
+        public static final int scan_qr_code=0x7f070026;
+        public static final int search_equipment=0x7f070025;
+        public static final int tableLayout1=0x7f070001;
+        public static final int tableRow1=0x7f070002;
+        public static final int tableRow2=0x7f070004;
+        public static final int tableRow3=0x7f070007;
+        public static final int tableRow4=0x7f07000a;
+        public static final int tableRow5=0x7f070010;
+        public static final int tableRow6=0x7f070012;
+        public static final int tableRow7=0x7f070014;
+        public static final int tableRow8=0x7f070016;
+        public static final int tableRow9=0x7f07000d;
+        public static final int textView1=0x7f070003;
+        public static final int textView10=0x7f070006;
+        public static final int textView11=0x7f070009;
+        public static final int textView12=0x7f07000c;
+        public static final int textView13=0x7f07000f;
+        public static final int textView2=0x7f070005;
+        public static final int textView3=0x7f070008;
+        public static final int textView4=0x7f07000b;
+        public static final int textView5=0x7f070011;
+        public static final int textView6=0x7f070013;
+        public static final int textView7=0x7f070015;
+        public static final int textView8=0x7f070017;
+        public static final int textView9=0x7f07000e;
     }
     public static final class layout {
-        public static final int activity_cidde_how_to=0x7f030000;
-        public static final int activity_cidde_main=0x7f030001;
-        public static final int activity_view_pager=0x7f030002;
-        public static final int view_pager_background=0x7f030003;
+        public static final int activity_cidde_help=0x7f030000;
+        public static final int activity_cidde_how_to=0x7f030001;
+        public static final int activity_cidde_main=0x7f030002;
+        public static final int activity_cidde_submit_bug_report=0x7f030003;
+        public static final int activity_cidde_troubleshoot=0x7f030004;
+        public static final int activity_view_pager=0x7f030005;
+        public static final int view_pager_background=0x7f030006;
     }
     public static final class menu {
-        public static final int activity_cidde_how_to=0x7f060000;
-        public static final int activity_cidde_main=0x7f060001;
-        public static final int activity_view_pager=0x7f060002;
+        public static final int activity_cidde_help=0x7f060000;
+        public static final int activity_cidde_how_to=0x7f060001;
+        public static final int activity_cidde_main=0x7f060002;
+        public static final int activity_cidde_submit_bug_report=0x7f060003;
+        public static final int activity_cidde_troubleshoot=0x7f060004;
+        public static final int activity_view_pager=0x7f060005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040003;
         public static final int menu_settings=0x7f040004;
+        public static final int title_activity_cidde_help=0x7f040006;
+        public static final int title_activity_cidde_submit_bug_report=0x7f040007;
+        public static final int title_activity_cidde_troubleshoot=0x7f040005;
         public static final int title_activity_how_to=0x7f040001;
         public static final int title_activity_how_to_view_pager=0x7f040002;
     }
