7675a6df8ceedcbcbed2ac9d968fdc16ba78ea1a
Tim O <tdoshea90@yahoo.com>
2013-04-14 16:04:13 -0400

    new classrooms and contact us
    all classrooms have been added. contact us has been updated.

--- src/edu/pitt/cidde/portico/CiddeBrowseRooms.java
+++ src/edu/pitt/cidde/portico/CiddeBrowseRooms.java
@@ -15,41 +15,491 @@ public class CiddeBrowseRooms extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_browse_rooms);
 
-		final Button button1 = (Button) findViewById(R.id.room1);
-		button1.setOnClickListener(new View.OnClickListener() {
+		final Button button113 = (Button) findViewById(R.id.czech_button);
+		button113.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
-				Intent howToIntent = new Intent(
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 113);
+
+				Intent classroomIntent = new Intent(
 						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
 						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
-				startActivity(howToIntent);
 
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
 			}
-		});
-
-		final Button button2 = (Button) findViewById(R.id.room2);
-		button2.setOnClickListener(new View.OnClickListener() {
+		}); // end of button
+		
+		final Button button116 = (Button) findViewById(R.id.italian_button);
+		button116.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
-				Intent howToIntent = new Intent(
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 116);
+
+				Intent classroomIntent = new Intent(
 						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
 						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
-				startActivity(howToIntent);
 
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
 			}
-		});
+		}); // end of button
 
-		final Button button3 = (Button) findViewById(R.id.room3);
-		button3.setOnClickListener(new View.OnClickListener() {
+		final Button button119 = (Button) findViewById(R.id.german_button);
+		button119.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
-				Intent howToIntent = new Intent(
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 119);
+
+				Intent classroomIntent = new Intent(
 						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
 						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
-				startActivity(howToIntent);
 
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
 			}
-		});
+		}); // end of button
+		
+		final Button button121 = (Button) findViewById(R.id.hungarian_button);
+		button121.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 121);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button126 = (Button) findViewById(R.id.polish_button);
+		button126.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 126);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button127 = (Button) findViewById(R.id.irish_button);
+		button127.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 127);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button129 = (Button) findViewById(R.id.lith_button);
+		button129.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 129);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button130 = (Button) findViewById(R.id.romanian_button);
+		button130.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 130);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button135 = (Button) findViewById(R.id.swedish_button);
+		button135.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 135);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button136 = (Button) findViewById(R.id.chinese_button);
+		button136.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 136);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button137 = (Button) findViewById(R.id.greek_button);
+		button137.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 137);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button139 = (Button) findViewById(R.id.scottish_button);
+		button139.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 139);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+
+		final Button button142 = (Button) findViewById(R.id.yugoslav_button);
+		button142.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 142);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button144 = (Button) findViewById(R.id.english_button);
+		button144.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 144);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button149 = (Button) findViewById(R.id.french_button);
+		button149.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 149);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button151 = (Button) findViewById(R.id.norwegian_button);
+		button151.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 151);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button153 = (Button) findViewById(R.id.russian_button);
+		button153.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 153);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button160 = (Button) findViewById(R.id.syria_button);
+		button160.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 160);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button314 = (Button) findViewById(R.id.austrian_button);
+		button314.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 314);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button317 = (Button) findViewById(R.id.jap_button);
+		button317.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 317);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button319 = (Button) findViewById(R.id.armenian_button);
+		button319.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 319);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button327 = (Button) findViewById(R.id.india_button);
+		button327.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 327);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button328 = (Button) findViewById(R.id.early_am_button);
+		button328.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 328);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button330 = (Button) findViewById(R.id.africa_button);
+		button330.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 330);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button337 = (Button) findViewById(R.id.israel_button);
+		button337.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 337);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button341 = (Button) findViewById(R.id.ukranian_button);
+		button341.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 341);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
+		
+		final Button button342 = (Button) findViewById(R.id.welsh_button);
+		button342.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				// DETERMINE ROOM
+				Bundle extras = new Bundle();
+				extras.putInt("room", 342);
+
+				Intent classroomIntent = new Intent(
+						edu.pitt.cidde.portico.CiddeBrowseRooms.this,
+						edu.pitt.cidde.portico.ClassRoomInfoActivity.class);
+
+				classroomIntent.putExtras(extras);
+
+				startActivity(classroomIntent);
+			}
+		}); // end of button
 	}// end onCreate method
 
 	@Override
--- src/edu/pitt/cidde/portico/CiddeHelpActivity.java
+++ src/edu/pitt/cidde/portico/CiddeHelpActivity.java
@@ -1,8 +1,13 @@
 package edu.pitt.cidde.portico;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class CiddeHelpActivity extends Activity {
 
@@ -10,6 +15,174 @@ public class CiddeHelpActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cidde_help);
+
+		Button emergencyButton;
+		Button generalButton;
+		Button requestsButton;
+		Button scheduleButton;
+		Button recordingButton;
+		Button lateButton;
+		Button otherButton;
+
+		Button etcButton;
+		Button facultyHelpButton;
+		Button gabeButton;
+		Button ashleyButton;
+		Button adawgButton;
+		Button willButton;
+		Button bettyButton;
+		Button blaineButton;
+
+		// Blaine button
+		blaineButton = (Button) findViewById(R.id.blaine_button);
+		blaineButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.blaine_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Betty button
+		bettyButton = (Button) findViewById(R.id.betty_button);
+		bettyButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.betty_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Will button
+		willButton = (Button) findViewById(R.id.will_button);
+		willButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.will_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// A dawg button
+		adawgButton = (Button) findViewById(R.id.adawg_button);
+		adawgButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.adawg_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Ashley button
+		ashleyButton = (Button) findViewById(R.id.ashley_button);
+		ashleyButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.ashley_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Gabe button
+		gabeButton = (Button) findViewById(R.id.gabe_button);
+		gabeButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.gabe_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Faculty button
+		facultyHelpButton = (Button) findViewById(R.id.faculty_help_button);
+		facultyHelpButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.faculty_help_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// ETC button
+		etcButton = (Button) findViewById(R.id.etc_button);
+		etcButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.etc_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Other button
+		otherButton = (Button) findViewById(R.id.other_button);
+		otherButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.other_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Late button
+		lateButton = (Button) findViewById(R.id.late_button);
+		lateButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.late_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Recording button
+		recordingButton = (Button) findViewById(R.id.recording_button);
+		recordingButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.recording_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Schedule button
+		scheduleButton = (Button) findViewById(R.id.schedule_button);
+		scheduleButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.schedule_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Requests button
+		requestsButton = (Button) findViewById(R.id.requests_button);
+		requestsButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.requests_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// General button
+		generalButton = (Button) findViewById(R.id.general_button);
+		generalButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.main_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
+		// Emergency button
+		emergencyButton = (Button) findViewById(R.id.emergency_button);
+		emergencyButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String number = getString(R.string.emergency_string);
+				startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(number)));
+			} // end of listener method
+		}); // end of anon listener class
+
 	}
 
 	@Override
